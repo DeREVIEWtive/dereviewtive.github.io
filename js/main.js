0bb7b23d2b84b50b4ca221381bc75bf38e4e4eea
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2025-08-14 09:27:08 +0800

    eeeee

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-// const click = document.querySelector("click");
-// click.addEventListener("click", () => {
-//     result.style.display = "block";
-// });
+window.onload = async () => {
+    const URLSearch = new URLSearchParams(window.location.search);
+    const lesson = decodeURIComponent(URLSearch.get("lesson"));
+
+    const lessonRequest = await fetch(`/lessons/${lesson}`);
+    const lessonData = await lessonRequest.text();
+
+    const breakIndex = lessonData.indexOf("\n") + 2;
+    const headerText = lessonData.slice(0, breakIndex);
+    const content = lessonData.slice(breakIndex + 1);
+
+    header.textContent = headerText;
+    main.innerHTML = marked.parse(content);
+};
